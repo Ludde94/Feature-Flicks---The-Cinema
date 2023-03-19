241d7228863e5016a13673e96b4f28cc9d1a261a
ludde94 <l.jenisch@hotmail.com>
2023-03-19 16:03:25 +0100

    added filtering by .cat

--- src/components/screening.jsx
+++ src/components/screening.jsx
@@ -11,6 +11,7 @@ const Screening = (props) => {
         <Card.Title>{props.movieTitle}</Card.Title>
         <Card.Text>{props.time}</Card.Text>
         <Card.Text>{auditoriumId}</Card.Text>
+        <Card.Text>{props.category.join(", ")}</Card.Text>
         <Link to={`/DisplaySeats/${props.screeningId}`} className="btn btn-primary btn-sm"> this isnt working
           Booking
         </Link>
--- src/pages/screenings.jsx
+++ src/pages/screenings.jsx
@@ -4,11 +4,14 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import Screening from '../components/screening';
 
 const Screenings = () => {
   const [screenings, setScreenings] = useState([]);
   const [movies, setMovies] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -20,25 +23,42 @@ const Screenings = () => {
       const moviesData = await moviesResponse.json();
       setScreenings(screeningsData);
       setMovies(moviesData);
+      setCategories([...new Set(moviesData.map(movie => movie.description.categories).flat())]);
     })();
   }, []);
 
-  console.log(movies); //LOG ALL THE CATEGORIES
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+  }
+
+  let filteredScreenings = screenings;
+  if (selectedCategory) {
+    const filteredMovies = movies.filter(movie => movie.description.categories.includes(selectedCategory));
+    const filteredMovieIds = filteredMovies.map(movie => movie.id);
+    filteredScreenings = screenings.filter(screening => filteredMovieIds.includes(screening.movieId));
+  }
 
   return (
     <div>
       <Container fluid className="movieContainer">
         <Row xs={1} sm={2} md={3} lg={4} xl={5} xxl={6}>
-              {screenings.map(({ time, movieId, auditoriumId, categories }, index) => {
-        const movie = movies.find((movie) => movie.id === movieId);
-        return (
-          <Col key={index}>
-            <Card className="mb-4">
-            <Screening time={time} movieTitle={movie.title} auditoriumId={auditoriumId} categories={categories}/>
-            </Card>
-          </Col>
-        );
-      })}
+          {categories.map((category, index) => (
+            <Col key={index}>
+              <button className="categoryButton" onClick={() => handleCategorySelect(category)}>{category}</button>
+            </Col>
+          ))}
+        </Row>
+        <Row xs={1} sm={2} md={3} lg={4} xl={5} xxl={6}>
+          {filteredScreenings.map(({ time, movieId, auditoriumId }, index) => {
+            const movie = movies.find((movie) => movie.id === movieId);
+            return (
+              <Col key={index}>
+                <Card className="mb-4">
+                  <Screening time={time} movieTitle={movie.title} auditoriumId={auditoriumId} category={movie.description.categories} />
+                </Card>
+              </Col>
+            );
+          })}
         </Row>
       </Container>
     </div>
